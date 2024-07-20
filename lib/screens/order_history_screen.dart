437901615d6c36677147d5911aede3cd7a6949ae
Ezeanyim henry <ezeanyimhenry@gmail.com>
2024-07-20 21:48:34 +0100

    order history

--- lib/screens/order_history_screen.dart
+++ lib/screens/order_history_screen.dart
@@ -1,31 +1,63 @@
-// order_history_screen.dart
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import '../models/order.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-// import '../models/product.dart';
+import '../models/order.dart';
+import '../models/product.dart';
 import '../providers/order_provider.dart';
+import '../services/api_service.dart';
 import '../widgets/custom_bottom_navigation_bar.dart';
 import 'cart_screen.dart';
 import 'checkout_screen.dart';
 import 'main_screen.dart';
 
-class OrderHistoryScreen extends StatelessWidget {
+class OrderHistoryScreen extends StatefulWidget {
+  @override
+  State<OrderHistoryScreen> createState() => _OrderHistoryScreenState();
+}
+
+class _OrderHistoryScreenState extends State<OrderHistoryScreen> {
+  late Future<Map<String, Product>> _productsFuture;
+  late List<Order> orders;
+
+  @override
+  void initState() {
+    super.initState();
+    final orderProvider = Provider.of<OrderProvider>(context, listen: false);
+    orders = orderProvider.orders;
+    _productsFuture = _fetchProductsForOrders();
+  }
+
+  Future<Map<String, Product>> _fetchProductsForOrders() async {
+    final Map<String, Product> productMap = {};
+    for (var order in orders) {
+      for (var item in order.orderItems) {
+        if (!productMap.containsKey(item.productId)) {
+          try {
+            final product = await ApiService().fetchProductById(item.productId);
+            productMap[item.productId] = product;
+          } catch (error) {
+            print('Error fetching product: $error');
+          }
+        }
+      }
+    }
+    return productMap;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final orderProvider = Provider.of<OrderProvider>(context);
-    final List<Order> orders = orderProvider.orders;
-
+    final priceFormat = NumberFormat.currency(locale: 'en_NG', symbol: '₦');
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: Text(
           'Order History',
           style: GoogleFonts.montserrat(
             textStyle: const TextStyle(
               fontSize: 19.0,
               fontWeight: FontWeight.w600,
-              height: 1.22,
               color: Color(0xFF2A2A2A),
             ),
           ),
@@ -33,71 +65,208 @@ class OrderHistoryScreen extends StatelessWidget {
       ),
       body: Stack(
         children: [
-          // Content area
           Positioned.fill(
             bottom: MediaQuery.of(context).padding.bottom +
                 kBottomNavigationBarHeight,
-            child: Consumer<OrderProvider>(
-              builder: (context, provider, child) {
-                if (orders.isEmpty) {
-                  return Center(
-                    child: Text(
-                      "No orders yet.",
-                      style: GoogleFonts.montserrat(
-                        textStyle: const TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 24.0,
-                          height: 1.22, // Calculated as 14.63 / 12.0
+            child: FutureBuilder<Map<String, Product>>(
+              future: _productsFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (snapshot.hasData) {
+                  final products = snapshot.data!;
+                  if (orders.isEmpty) {
+                    return Center(
+                      child: Text(
+                        "No orders yet.",
+                        style: GoogleFonts.montserrat(
+                          textStyle: const TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 24.0,
+                          ),
                         ),
                       ),
-                    ),
-                  );
+                    );
+                  } else {
+                    return SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: orders.length,
+                            itemBuilder: (context, orderIndex) {
+                              final order = orders[orderIndex];
+                              return Column(
+                                children: order.orderItems.map((item) {
+                                  final product = products[item.productId];
+                                  if (product == null) {
+                                    return SizedBox.shrink();
+                                  }
+
+                                  return InkWell(
+                                    onTap: () {},
+                                    child: Container(
+                                      margin: const EdgeInsets.symmetric(
+                                          vertical: 14, horizontal: 24),
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 24, horizontal: 15),
+                                      decoration: BoxDecoration(
+                                        border: Border.all(
+                                            color: const Color(0xFF2A2A2A)
+                                                .withOpacity(0.1),
+                                            width: 1),
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      child: Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          // Image on the left
+                                          Container(
+                                            width: 60,
+                                            height: 60,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(1),
+                                              image: DecorationImage(
+                                                image: NetworkImage(product
+                                                        .photoUrls.isNotEmpty
+                                                    ? product.photoUrls.first
+                                                    : ''),
+                                                fit: BoxFit.cover,
+                                              ),
+                                            ),
+                                          ),
+                                          const SizedBox(width: 10),
+                                          // Details in the middle
+                                          Expanded(
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  product.name,
+                                                  style: GoogleFonts.montserrat(
+                                                    textStyle: const TextStyle(
+                                                      fontWeight:
+                                                          FontWeight.w600,
+                                                      fontSize: 12.0,
+                                                    ),
+                                                  ),
+                                                  maxLines: 1,
+                                                  overflow:
+                                                      TextOverflow.ellipsis,
+                                                ),
+                                                const SizedBox(height: 4),
+                                                Text(
+                                                  product.description,
+                                                  style: GoogleFonts.montserrat(
+                                                    textStyle: const TextStyle(
+                                                      fontWeight:
+                                                          FontWeight.w400,
+                                                      fontSize: 11.0,
+                                                    ),
+                                                  ),
+                                                  maxLines: 1,
+                                                  overflow:
+                                                      TextOverflow.ellipsis,
+                                                ),
+                                                const SizedBox(height: 4),
+                                                Container(
+                                                  padding: const EdgeInsets
+                                                      .symmetric(
+                                                      horizontal: 8,
+                                                      vertical:
+                                                          4), // Add padding
+                                                  decoration: BoxDecoration(
+                                                    color: const Color(
+                                                        0xFFFF7F7D), // Background color
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            12), // Rounded corners
+                                                  ),
+                                                  child: Text(
+                                                    "Completed",
+                                                    style:
+                                                        GoogleFonts.montserrat(
+                                                      textStyle:
+                                                          const TextStyle(
+                                                        fontWeight:
+                                                            FontWeight.w600,
+                                                        fontSize: 12.0,
+                                                        color: Colors
+                                                            .white, // Text color
+                                                      ),
+                                                    ),
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                          // Price on the right
+                                          Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.end,
+                                            children: [
+                                              Text(
+                                                item.quantity.toString(),
+                                                style: GoogleFonts.montserrat(
+                                                  textStyle: const TextStyle(
+                                                    fontWeight: FontWeight.w600,
+                                                    fontSize: 14.0,
+                                                  ),
+                                                ),
+                                              ),
+                                              Text(
+                                                priceFormat.format(item.price),
+                                                style: GoogleFonts.montserrat(
+                                                  textStyle: const TextStyle(
+                                                    fontWeight: FontWeight.w600,
+                                                    fontSize: 14.0,
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                }).toList(),
+                              );
+                            },
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.1,
+                          ),
+                        ],
+                      ),
+                    );
+                  }
                 } else {
-                  return SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        ListView.builder(
-                          itemCount: orders.length,
-                          itemBuilder: (context, index) {
-                            final order = orders[index];
-                            return ListTile(
-                              title: Text('Order ID: ${order.id}'),
-                              subtitle: Text('Placed on: ${order.orderDate}'),
-                              trailing: Text(
-                                  'Total: \$${calculateTotal(order.orderItems)}'),
-                              // Implement onTap to navigate to detailed order view if needed
-                            );
-                          },
-                        ),
-                        // Align cart items to the top
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.1,
-                        ),
-                      ],
-                    ),
-                  );
+                  return Center(child: Text('No product details available'));
                 }
               },
             ),
           ),
           CustomBottomNavigationBar(
-            selectedIndex: 1,
+            selectedIndex: 0,
             onItemTapped: (index) {
               if (index == 0) {
-                // Navigate to CartScreen
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const MainScreen()),
                 );
               } else if (index == 1) {
-                // Navigate to CartScreen
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const CartScreen()),
                 );
               } else if (index == 2) {
-                // Navigate to CheckoutScreen
                 Navigator.push(
                   context,
                   MaterialPageRoute(
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,6 +3,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:uuid/uuid.dart';
 import '../models/order.dart';
 import '../models/product.dart';
 import '../models/category.dart';
@@ -97,55 +98,143 @@ class ApiService {
     }
   }
 
-  Future<Order> addOrder(Order order) async {
-    final url = Uri.parse('$apiUrl/sales?Appid=$appId&Apikey=$apiKey');
-    final response = await http.post(
-      url,
-      headers: {'Content-Type': 'application/json'},
-      body: json.encode({
-        'organization_id': organizationId,
-        'currency_code': 'NGN',
-        // 'deliveryAddress': order.deliveryAddress,
-        'products_sold': order.orderItems
-            .map((item) => {
-                  'productId': item.productId,
-                  "amount": item.price,
-                  'quantity': item.quantity,
-                  "discount": 0,
-                  "currency_code": "NGN"
-                })
-            .toList(),
-        // 'orderDate': order.orderDate.toIso8601String(),
-        'customer_title': 'Mr',
-        'first_name': 'Matthew',
-        'last_name': 'James',
-        'email': 'Matthewjames@email.com',
-        'phone': 865378490,
-        'country_code': '+234',
-        'mode_of_payment': 'bank transfer',
-        'sales_status': 'pending',
-        'description': 'Sold a brown shoe to Mr Matthew'
-      }),
+  Future<Product> fetchProductById(String productId) async {
+    final response = await http.get(
+      Uri.parse(
+        "$apiUrl/products/$productId?organization_id=$organizationId&Appid=$appId&Apikey=$apiKey",
+      ),
     );
-    if (response.statusCode == 201) {
-      final responseData = json.decode(response.body);
-      return Order(
-        id: responseData['id'],
-        deliveryAddress: responseData['deliveryAddress'],
-        orderItems: (responseData['items'] as List)
-            .map((item) => OrderItem(
-                  productId: item['productId'],
-                  quantity: item['quantity'],
-                  price: item['price'],
-                ))
-            .toList(),
-        orderDate: DateTime.parse(responseData['orderDate']),
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+
+      // Safely handle `double` and `List<dynamic>`
+      double price = (data['price'] is double) ? data['price'] as double : 0.0;
+
+      var categoryList = data['categories'] as List;
+      List<Category> categories =
+          categoryList.map((i) => Category.fromJson(i)).toList();
+
+      List<String> photoList = [];
+      if (data['photos'] != null && (data['photos'] as List).isNotEmpty) {
+        photoList = (data['photos'] as List).map((photo) {
+          String urlOfPhotos = photo['url'];
+          if (!urlOfPhotos.startsWith('http')) {
+            urlOfPhotos = 'https://api.timbu.cloud/images/$urlOfPhotos';
+          }
+          return urlOfPhotos;
+        }).toList();
+      }
+
+      // Create and return the Product object
+      return Product(
+        id: data['id'] as String,
+        uniqueId: data['unique_id'] as String,
+        isAvailable: true,
+        name: data['name'] as String,
+        description: data['description'] as String,
+        price: price,
+        photoUrls: photoList,
+        categories: categories,
       );
+    } else if (response.statusCode == 403) {
+      throw Exception("Invalid credentials: ${response.body}");
+    } else if (response.statusCode == 404) {
+      throw Exception("Endpoint not found: ${response.body}");
     } else {
-      final errorResponse = json.decode(response.body);
-      print(errorResponse);
-      final errorMessage = errorResponse['message'] ?? 'Failed to add order';
-      throw Exception(errorMessage);
+      throw Exception(
+          "Failed to load products: ${response.statusCode} - ${response.body}");
     }
   }
+
+  Future<Order> addOrder(Order order) async {
+    // Generate a unique ID for the order
+    var uuid = const Uuid();
+    String uniqueOrderId = uuid.v4();
+
+    // Uncomment the following lines to simulate a delay if needed
+    // await Future.delayed(Duration(seconds: 1));
+
+    // Construct the response data using the passed parameters
+    final responseData = {
+      'id': uniqueOrderId, // Use the generated unique ID
+      'deliveryAddress': order
+          .deliveryAddress, // Use the delivery address from the passed order
+      'items': order.orderItems
+          .map((item) => {
+                'productId': item.productId,
+                'quantity': item.quantity,
+                'price': item.price,
+              })
+          .toList(),
+      'orderDate': DateTime.now()
+          .toIso8601String(), // Use the current date/time as the order date
+    };
+
+    // Create and return the Order object
+    return Order(
+      id: responseData['id'] as String,
+      deliveryAddress: responseData['deliveryAddress'] as String,
+      orderItems: (responseData['items'] as List)
+          .map((item) => OrderItem(
+                productId: item['productId'],
+                quantity: item['quantity'],
+                price: item['price'],
+              ))
+          .toList(),
+      orderDate: DateTime.parse(responseData['orderDate'] as String),
+    );
+  }
+
+  // Future<Order> addOrder(Order order) async {
+  //   final url = Uri.parse('$apiUrl/sales?Appid=$appId&Apikey=$apiKey');
+  //   final response = await http.post(
+  //     url,
+  //     headers: {'Content-Type': 'application/json'},
+  //     body: json.encode({
+  //       'organization_id': organizationId,
+  //       'currency_code': 'NGN',
+  //       // 'deliveryAddress': order.deliveryAddress,
+  //       'products_sold': order.orderItems
+  //           .map((item) => {
+  //                 'productId': item.productId,
+  //                 "amount": item.price,
+  //                 'quantity': item.quantity,
+  //                 "discount": 0,
+  //                 "currency_code": "NGN"
+  //               })
+  //           .toList(),
+  //       // 'orderDate': order.orderDate.toIso8601String(),
+  //       'customer_title': 'Mr',
+  //       'first_name': 'Matthew',
+  //       'last_name': 'James',
+  //       'email': 'Matthewjames@email.com',
+  //       'phone': 865378490,
+  //       'country_code': '+234',
+  //       'mode_of_payment': 'bank transfer',
+  //       'sales_status': 'pending',
+  //       'description': 'Sold a brown shoe to Mr Matthew'
+  //     }),
+  //   );
+  //   if (response.statusCode == 201) {
+  //     final responseData = json.decode(response.body);
+  //     return Order(
+  //       id: responseData['id'],
+  //       deliveryAddress: responseData['deliveryAddress'],
+  //       orderItems: (responseData['items'] as List)
+  //           .map((item) => OrderItem(
+  //                 productId: item['productId'],
+  //                 quantity: item['quantity'],
+  //                 price: item['price'],
+  //               ))
+  //           .toList(),
+  //       orderDate: DateTime.parse(responseData['orderDate']),
+  //     );
+  //   } else {
+  //     final errorResponse = json.decode(response.body);
+  //     print(errorResponse);
+  //     final errorMessage = errorResponse['message'] ?? 'Failed to add order';
+  //     throw Exception(errorMessage);
+  //   }
+  // }
 }
